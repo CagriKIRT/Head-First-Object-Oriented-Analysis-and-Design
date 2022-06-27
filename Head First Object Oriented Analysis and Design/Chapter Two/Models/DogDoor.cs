ff9631ac3d12c28979e759e9505e0e81cb140969
cagrikirt <cagrikirt@gmail.com>
2022-06-27 20:15:13 +0300

    create dogdoor model

---
+++ Head First Object Oriented Analysis and Design/Chapter Two/Models/DogDoor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Head_First_Object_Oriented_Analysis_and_Design.Chapter_Two.Models
+{
+    public class DogDoor
+    {
+        private bool open;
+
+        public DogDoor()
+        {
+            this.open = false;
+        }
+
+        public void openDoor()
+        {
+            Console.WriteLine("The dog doors opens");
+            open = true;
+        }
+
+        public void close()
+        {
+            Console.WriteLine("The dog door closes");
+            open = false;
+        }
+
+        public bool isOpen() => open;
+    }
+}
