6f819c0f0409393863d4218d287181adc5f71b20
cagrikirt <cagrikirt@gmail.com>
2022-06-27 20:10:01 +0300

    Add project files.

---
+++ Head First Object Oriented Analysis and Design/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Head_First_Object_Oriented_Analysis_and_Design
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
